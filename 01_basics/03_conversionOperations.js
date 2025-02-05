2de4f27502209c7502165266d82f357a216e2622
Joydeb roy <104813131+JoydebRoy01@users.noreply.github.com>
2025-02-05 13:40:10 +0000

    comparision

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -12,12 +12,44 @@ let score = "33"
 
 let isLoggedIn = 1 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 //"" => false
 
 let someNumber = 33
 
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+/********************** Operations************************/
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2%2);
+// console.log(2/2);
+
+let str1="hello"
+let str2="joydeb"
+
+let str3=str1+str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+let gameCounter = 10
+++gameCounter
+console.log(gameCounter);
+
+
+
+
