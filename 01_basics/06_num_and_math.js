4ce570968b3cb0e09e68554c077ae5958a6e52e7
Joydeb roy <104813131+JoydebRoy01@users.noreply.github.com>
2025-02-12 11:02:37 +0000

    06_num_math

---
+++ 01_basics/06_num_and_math.js
@@ -0,0 +1,36 @@
+const score = 400
+// console.log(score);
+
+
+const balance = new Number(100)
+// console.log (balance);
+
+console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+const otherNumber = 123.8966
+// console.log(otherNumber.toPrecision(3));
+
+const hunderds = 100000
+// console.log(hunderds.toLocaleString('en-IN'));
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(3.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4,4,2,5,7));
+// console.log(Math.max(4,4,2,5,7));
+
+console.log(Math.random());
+console.log(Math.floor(Math.random()*10)+1);
+
+const min =10
+const max = 20
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+ 
+
+
+
+
+
