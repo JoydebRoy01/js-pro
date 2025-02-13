02068e5061bd45b2123f49d90d889780326c1c50
Joydeb roy <104813131+JoydebRoy01@users.noreply.github.com>
2025-02-13 15:00:38 +0000

    Objects

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,40 @@
+//singlelon
+// const InstaUser = new Object()
+
+const InstaUser = {} //non singleton objects
+
+InstaUser.id = "123abc"
+InstaUser.name = "Joydeb"
+InstaUser.isLoggedIn = false
+
+
+const User = {
+    email:"joy@81",
+    fullname:{
+        userFullName:{
+            firstName:"Joydeb",
+            lastName:"roy"
+        }
+    }
+}
+// console.log(User.fullname.userFullName.firstName);
+
+const obj1 = {1:"a", 2:"b"}
+const obj2 = {3:"a", 4:"b"}
+
+// const obj3 = Object.assign({}, obj1,obj2)
+
+const obj3 = {...obj1, ...obj2} //splate operator
+
+// console.log(obj3);
+
+// users[1].email
+console.log(InstaUser);
+
+console.log(Object.keys(InstaUser));
+console.log(Object.values(InstaUser));
+console.log(Object.entries(InstaUser));
+
+console.log(InstaUser.hasOwnProperty('isLoggedIn'));
+
+
