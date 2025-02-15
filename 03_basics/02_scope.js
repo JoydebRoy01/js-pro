14b49e7a677c91a98dd831bc98af16e1bbce1bc2
Joydeb roy <104813131+JoydebRoy01@users.noreply.github.com>
2025-02-15 09:20:23 +0000

    Scope

---
+++ 03_basics/02_scope.js
@@ -0,0 +1,53 @@
+if (true){
+    let a=10
+    const b=20
+    var c=30
+}
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+function one(){
+    const  userName = "joydeb"
+
+    function two(){
+        const website = "github"
+        console.log(userName);
+        
+    }
+    // console.log(website);
+
+    two()
+    
+}
+// one()
+
+if (true){
+    const userName = "joydeb"
+    if (userName === "joydeb"){
+        const website = "github"
+        // console.log(userName , website);
+        
+    }
+    // console.log(website);
+    
+}
+
+// console.log(userName);
+
+
+//++++++++++++ **  +++++++++++
+
+function addOne(num){
+    return num+ 1
+
+}
+console.log(addOne(4));
+ 
+
+const addTwo = function(num){
+    return num+2
+}
+
+addTwo (4)
