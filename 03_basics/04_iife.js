4cf12f69706532e06e87ecabe3921e331af26832
Joydeb roy <104813131+JoydebRoy01@users.noreply.github.com>
2025-02-16 06:41:41 +0000

    IIFE

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,14 @@
+//Immediately Invoked Funvtion Expressions(IIFE)
+
+
+(function coffee(){
+    console.log("vonnect");
+
+    
+})();
+
+(  (name) => {
+    console.log(`New Connections ${name}`);
+    
+})('joydeb')
+
